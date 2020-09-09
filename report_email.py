f59ce64433b0920fd29fc0268f015d4074237f74
Elliot J. Voris <elliot@voris.me>
2020-09-08 21:59:12 -0500

    I think I got a pretty good start on the reports/emails business. Phew!

--- report_email.py
+++ report_email.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
-# Import all the necessary libraries(os, datetime and reports) that will be used to process the text data from the supplier-data/descriptions directory into the format below:
+# Import all the necessary libraries(os, datetime and reports) that will
+# be used to process the text data from the supplier-data/descriptions
+# directory into the format below:
+#
 # name: Apple
 # weight: 500 lbs
 # [blank line]
@@ -18,3 +21,46 @@
 # Subject line: Upload Completed - Online Fruit Store
 # E-mail Body: All fruits are uploaded to our website successfully. A detailed list is attached to this email.
 # Attachment: Attach the path to the file processed.pdf
+
+import reports
+import emails
+
+import os
+import datetime
+
+def generate_paragraph(data_list):
+    paragraph = ""
+    for fruit in data_list:
+        paragraph += 'name: ' + fruit['name'] + '\nweight: ' + fruit['weight'] + '\n\n'
+    return paragraph
+
+def main():
+    desc_dir = "supplier-data/descriptions/"
+    desc_files = os.listdir(desc_dir)
+    attachment = "/tmp/processed.pdf"
+    title = "Processed on " + str(datetime.datetime.now().strftime("%Y-%m-%d")) + '\n'
+
+    email_from = "automation@example.com"
+    email_to = "username@example.com"
+    email_subject = "Upload Completed - Online Fruit Store"
+    email_body = "All fruits are uploaded to our website successfully. A detailed list is attached to this email."
+
+    supplier_data_list = []
+
+    for file in desc_files:
+        fruit_data = {}
+        with open(desc_dir + file) as f:
+            fruit_data['name'] = f.readline().rstrip('\n')
+            fruit_data['weight'] = f.readline().rstrip('\n')
+            supplier_data_list.append(fruit_data)
+
+    report = reports.generate_report(attachment, title, generate_paragraph(supplier_data_list))
+
+    emails.send_email(emails.generate_email(email_from,
+                                            email_to,
+                                            subject_line,
+                                            email_body,
+                                            attachment))
+
+if __name__ == "__main__":
+  main()
--- reports.py
+++ reports.py
@@ -16,3 +16,14 @@
 # to build the PDF reports. We have already covered how to generate PDF
 # reports in an earlier lesson; you will want to use similar concepts to
 # create a PDF report named processed.pdf.
+
+from reportlab.platypus import SimpleDocTemplate
+from reportlab.platypus import Paragraph, Spacer, Table, Image
+from reportlab.lib.styles import getSampleStyleSheet
+
+def generate_report(attachment, title, paragraph):
+    styles = getSampleStyleSheet()
+    report = SimpleDocTemplate(attachment)
+    report_title = Paragraph(title, styles["h1"])
+    empty_line = Spacer(1,20)
+    report.build([report_title, empty_line, paragraph])
